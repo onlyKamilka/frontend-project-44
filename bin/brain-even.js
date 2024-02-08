b316b9465e3fa4408db1e59ff3ab5dee7a4b26b7
kamil <kamilxai009@gmail.com>
2024-02-08 16:14:31 +0300

    f

---
+++ bin/brain-even.js
@@ -0,0 +1,16 @@
+import readlineSync from 'readline-sync';
+
+const brainEven = () => {
+  const game = 'even';
+  const name = startGame('Answer "yes" if the number is even, otherwise answer "no".');
+  bodyGame(name, () => {
+    const number = getNumber();
+    showQuestion(game, number);
+    const answer = readlineSync.question('Your answer: ');
+    const message = checkAnswer(name, answer, () => (number % 2 === 0 ? 'yes' : 'no'));
+    showMessage(message);
+    return message;
+  });
+};
+
+export default brainEven;
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,5 @@
-console.log('Welcome to the Brain Games!');
+#!/usr/bin/env node
+
+import greetings from 'src/cli.js';
+
+greetings();
